daa2abd1bf6654ec40ccebf5e86171ea9a497bae
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-01-31 22:55:21 -0500

    Update README.r

--- README.r
+++ README.r
@@ -1,6 +1,14 @@
-# first_r_repo
+# first_r_repo excercise is from codecademy I did
 
 library(readr)
 library(dplyr)
 
 orders <- read_csv("orders.csv")
+
+head(orders)
+
+diff_from_mean <- orders %>%
+  group_by(shoe_type) %>%
+  mutate(diff_from_shoe_type_mean = price - mean(price, na.rm = TRUE))
+
+diff_from_mean
